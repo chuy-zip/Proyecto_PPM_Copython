82350db5d4762ff7fe4c1a087d1e34396ff87f9d
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2023-09-05 08:52:50 -0600

    Added Color Palette

--- app/src/main/java/com/example/copython/ui/theme/ui/theme/Color.kt
+++ app/src/main/java/com/example/copython/ui/theme/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//Color Palette
+val Blue10 = Color(0xFF023E8A)
+val DarkBlue10 = Color(0xFF003566)
+val LightBlue10 = Color(0xFF549FFF)
+val Yellow10 = Color(0xFFFFC300)
+val OragneYellow = Color(0xFFFF9900)
