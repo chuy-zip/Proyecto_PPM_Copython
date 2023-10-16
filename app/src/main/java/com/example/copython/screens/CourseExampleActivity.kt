06ffb0e1015252a943eae6a5a34514887e8fd80f
chuy-zip <andreschuy03@gmail.com>
2023-10-15 19:58:30 -0600

    Added scaffold w topbar in course activity

--- app/src/main/java/com/example/copython/screens/CourseExampleActivity.kt
+++ app/src/main/java/com/example/copython/screens/CourseExampleActivity.kt
@@ -1,50 +1,91 @@
 package com.example.copython.screens
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.copython.ui.theme.ui.theme.Blue10
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CourseExampleLayout(navController: NavController){
-    Column(
-        modifier = Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceBetween,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        ScrollableTextsColumn()
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+
+    Scaffold (
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = Blue10,
+                    titleContentColor = Color.White,
+                ),
+                title = {
+                    Text(
+                        "Hello World",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack()}) {
+                        Icon(
+                            imageVector = Icons.Filled.KeyboardArrowLeft,
+                            contentDescription = "Localized description",
+                            tint = Color.White
+                        )
+                    }
+                },
+            )
+        }
+
+    ){ innerPadding ->
+        ScrollableTextsColumn(innerPadding)
     }
+//    Column(
+//        modifier = Modifier
+//            .fillMaxSize(),
+//        verticalArrangement = Arrangement.SpaceBetween,
+//        horizontalAlignment = Alignment.CenterHorizontally
+//    ){
+//        ScrollableTextsColumn()
+//    }
 
 }
 
 @Composable
-fun ScrollableTextsColumn(){
+fun ScrollableTextsColumn(innerPadding: PaddingValues) {
     Column (
         modifier = Modifier
-            .padding(30.dp)
-            .height(570.dp)
+            .padding(innerPadding)
+            .padding(16.dp)
             .verticalScroll(rememberScrollState())
     ){
         Text(
@@ -83,11 +124,20 @@ fun ScrollableTextsColumn(){
 
         Text(
             text = """
-                
                 This program prints the text "Hello, World!" to the console. It's often the first program beginners write when learning a new programming language.
             """.trimIndent(),
             fontSize = 16.sp,
             color = Color.Black
         )
+
+        Box( modifier = Modifier
+            .height(500.dp)
+            .fillMaxWidth()
+            .background(color = Color.LightGray, shape = RoundedCornerShape(16.dp))
+        ){
+            Text(
+                text = " Caja de prueba para el scroll"
+            )
+        }
     }
 }
