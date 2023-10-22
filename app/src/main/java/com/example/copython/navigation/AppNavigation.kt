7d4fd8b56ac1a8d3204129497f740058dba34216
Dan <pen22217@uvg.edu.gt>
2023-10-21 19:58:46 -0600

    Removed unused imports

--- app/src/main/java/com/example/copython/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/copython/navigation/AppNavigation.kt
@@ -6,12 +6,10 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.copython.screens.AIChatLayout
 import com.example.copython.screens.CourseExampleLayout
 import com.example.copython.screens.LoginLayout
 import com.example.copython.screens.MainMenuLayout
 import com.example.copython.screens.SignupLayout
-import com.example.copython.screens.UserLayout
 
 @Composable
 fun AppNavigation(){
